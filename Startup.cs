1466f2cd062b29815bdc8fc2248bcde0701df032
Ricky Hartmann <hartmannr76@gmail.com>
2018-07-06 04:35:04 -0400

    Cleanup

--- Startup.cs
+++ Startup.cs
@@ -19,8 +19,7 @@ namespace dotnet_cra_ssr
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-            .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
